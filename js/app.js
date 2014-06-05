604e95ef99e2c99b9c1af7850d238ff64d04c577
zconn08 <zconn@unknowna8bbcf21d658.gateway.pace.com>
2014-06-05 03:36:16 -0700

    Part 4

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$(".ryu").mouseenter(function(){
+		$(".ryu-still").hide();
+		$(".ryu-ready").show();
+	})
+	.mouseleave(function(){
+		$(".ryu-ready").hide();
+		$(".ryu-still").show();
+	})
+	.mousedown(function(){
+		playHadouken();
+		$(".ryu-ready").hide();
+		$(".ryu-throwing").show();
+		$(".hadouken").finish().show()
+		.animate({"left":"300px"},500,function(){
+			$(this).hide();
+			$(this).css("left","-212px");
+		});
+	})
+	.mouseup(function(){
+		$(".ryu-throwing").hide();
+		$("ryu-ready").show();
+	});
+});
+
+function playHadouken () {
+	$("#hadouken-sound")[0].volume = 0.5;
+	$("#hadouken-sound")[0].load();
+	$("#hadouken-sound")[0].play();
+}
